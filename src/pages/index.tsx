eaadc873a8f0aa1b37b6686135ad466b6c469171
tgmrks <tgmrks@gmail.com>
2022-03-25 17:41:51 -0300

    SSG implemented

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
+//import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
 import { stripe } from '../services/stripe'
@@ -34,7 +35,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KgaX8A6rsZzZ9rDFBdY7nou', //Pricing API ID (Products > Pricing)
   {
     expand: ['product']
@@ -51,6 +52,27 @@ export const getServerSideProps: GetServerSideProps = async () => {
     return {
       props: {
         product,
-      }
+      },
+      revalidate: 60 * 60 * 24, //24 hours
     }
 }
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const price = await stripe.prices.retrieve('price_1KgaX8A6rsZzZ9rDFBdY7nou', //Pricing API ID (Products > Pricing)
+//   {
+//     expand: ['product']
+//   });
+
+//   const product = {
+//     priceId: price.id,
+//     amount: new Intl.NumberFormat('en-US', {
+//       style: 'currency',
+//       currency: 'USD',
+//     }).format(price.unit_amount /100),
+//   }
+ 
+//     return {
+//       props: {
+//         product,
+//       }
+//     }
+// }
