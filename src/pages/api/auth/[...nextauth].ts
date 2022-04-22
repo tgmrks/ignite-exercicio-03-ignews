a3dbac607aa60b9a8205628638da0f4e7c121092
tgmrks <tgmrks@gmail.com>
2022-04-22 16:08:04 -0300

    config faunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from 'faunadb'
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -8,11 +11,55 @@ export default NextAuth({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       authorization: {
-          params: {
-            scope: 'read:user',
-          }
+        params: {
+          scope: 'read:user',
+        }
       },
     }),
     // ...add more providers here
   ],
+  jwt: {
+    secret: process.env.SIGNING_KEY
+  },
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      //const signInEmail = user.email
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'), q.Casefold(user.email)
+                )//Match 
+              )//Exists
+            ),//Not
+            q.Create(
+              q.Collection('users'), { data: { email: user.email } }
+            ),//Create
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )//Match 
+            )//Get
+          )//If
+        )//query
+        return true
+      } catch {
+        return false
+      }
+      // try {
+      //   await fauna.query(
+      //     q.Create(
+      //       q.Collection('users'),
+      //       { data: { signInEmail } }
+      //       )
+      //     )
+      //     return true
+      // } catch {
+      //   return false
+      // }
+    },
+  }
 })
