c57cf7a0dce420175bf6a0e1c01aba5afc6c39c4
tgmrks <tgmrks@gmail.com>
2022-07-04 08:22:40 -0300

    treating webhook messages and persisting data with fauna

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripesJs } from '../../services/stripes-js';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -5,8 +8,33 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton( { priceId }: SubscribeButtonProps) {
+
+    //const [session]: any = useSession();
+    const { data: session} = useSession();
+
+    async function handleSubscribe() {
+        if(!session) {
+            signIn('github')
+            return;
+        }
+        //creat checkout session with Stipe
+        try {
+            const response = await api.post('/subscribe') //o nome do arquivo eh o nome da rota
+            const { sessionId } = response.data;
+            const stripe = await getStripesJs();
+            await stripe.redirectToCheckout({sessionId: sessionId});
+        }
+        catch (err) {
+            alert(err.message)
+        }
+    }
+
     return (
-        <button type="button" className={styles.subscribeButton}>
+        <button 
+            type="button" 
+            className={styles.subscribeButton}
+            onClick={handleSubscribe}
+        >
             Subscribe now
         </button>
     )
